b2330dfe8e1aa40ade02e75461c605926b1144ba
Mohamed Elashri <mail@elashri.com>
2023-12-22 17:41:08 -0500

    update again

--- app.py
+++ app.py
@@ -8,76 +8,72 @@ import xmltodict
 from tabulate import tabulate
 import pandas as pd
 
-class MubasherAPI :
-  def __init__ (self,country):
-    self.ROOT = "/content/"
-    self.HostURL= "http://www.mubasher.info"
-    self.CompaniesAPI="/api/1/listed-companies"
-    self.PricesAPI = "/api/1/stocks/prices/all"
-    self.performanceApi = "/api/1/analysis/performance-comparison/stock?query="
-    self.HistoricalDirectory = self.ROOT+"historical_data/"
-    self.CompaniesDirectory=self.ROOT+"companies_data/"
-    self.outputFile =self.CompaniesDirectory+ "companies_database_"+ country+".xml"
-    self.dataBase = {}
-    self.country = self._validateCountry(country)
-    self.lastPrices = {}
-    self.dataDownloaded = False
+class MubasherAPI:
+    def __init__(self, country):
+        self.ROOT = "content/"
+        self.HostURL = "http://www.mubasher.info"
+        self.CompaniesAPI = "/api/1/listed-companies"
+        self.PricesAPI = "/api/1/stocks/prices/all"
+        self.performanceApi = "/api/1/analysis/performance-comparison/stock?query="
+        self.HistoricalDirectory = self.ROOT + "historical_data/"
+        self.CompaniesDirectory = self.ROOT + "companies_data/"
+        self.outputFile = self.CompaniesDirectory + "companies_database_" + country + ".xml"
+        self.dataBase = {}
+        self.country = self._validateCountry(country)
+        self.lastPrices = {}
+        self.dataDownloaded = False
 
+    def _ensure_directory(self, directory):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-  def _validateCountry(self,country):
-    if not country in ["eg","sa","ae","qa","bh","om","kw","jo","tn","ma","ps","iq"]:
-      raise ValueError("wrong country code, please use listCountries to see available country codes")
-    return country
+    def _validateCountry(self, country):
+        if not country in ["eg", "sa", "ae", "qa", "bh", "om", "kw", "jo", "tn", "ma", "ps", "iq"]:
+            raise ValueError("wrong country code, please use listCountries to see available country codes")
+        return country
 
-  def _GetCompanies(self):
-    currentPage = 0
-    allPages = 20
-    pageSize = 20
-    companiesNumber =int(requests.get(self.HostURL + self.CompaniesAPI,params={'country':self.country,'size':1}).json()["numberOfPages"])
-    while allPages>=currentPage:
-      ploads = {'country':self.country,'size':pageSize,'start':currentPage*pageSize}
-      r = requests.get(self.HostURL + self.CompaniesAPI,params=ploads)
-      response = r.json()
-      allPages =int(response["numberOfPages"])
-      for i in range(len(response["rows"])):
-        
-        company=response["rows"][i]
-        print("importing companies :" + str(i+currentPage*pageSize)+"/"+str(companiesNumber))
-        dataElement = {"name":company["name"],
-                      "url":self.HostURL+company["url"],
-                      "csv":self.getHistoricalFile(company),
-                      "symbol":company["symbol"]}
-        self.dataBase[company["symbol"]]=dataElement
-      currentPage=currentPage+1
-    xmlData = dicttoxml(self.dataBase.values()).decode()
-    if not (os.path.exists(self.CompaniesDirectory)):
-      os.mkdir(self.CompaniesDirectory)
-    with open(self.outputFile , "w") as file_object:
-      file_object.write(xmlData)
-    
-    print("Import complete, saved to : "+self.outputFile )
+    def _GetCompanies(self):
+        self._ensure_directory(self.CompaniesDirectory)  # Ensure the directory exists
+        currentPage = 0
+        allPages = 20
+        pageSize = 20
+        companiesNumber = int(requests.get(self.HostURL + self.CompaniesAPI, params={'country': self.country, 'size': 1}).json()["numberOfPages"])
+        while allPages >= currentPage:
+            ploads = {'country': self.country, 'size': pageSize, 'start': currentPage * pageSize}
+            r = requests.get(self.HostURL + self.CompaniesAPI, params=ploads)
+            response = r.json()
+            allPages = int(response["numberOfPages"])
+            for i in range(len(response["rows"])):
+                company = response["rows"][i]
+                print("importing companies :" + str(i + currentPage * pageSize) + "/" + str(companiesNumber))
+                dataElement = {"name": company["name"],
+                               "url": self.HostURL + company["url"],
+                               "csv": self.getHistoricalFile(company),
+                               "symbol": company["symbol"]}
+                self.dataBase[company["symbol"]] = dataElement
+            currentPage = currentPage + 1
+        xmlData = dicttoxml(self.dataBase.values()).decode()
+        with open(self.outputFile, "w") as file_object:
+            file_object.write(xmlData)
 
-  def DownloadHistorical(self,company):
-    if not (os.path.exists(self.HistoricalDirectory)):
-      os.mkdir(self.HistoricalDirectory)
-      os.mkdir(self.HistoricalDirectory+self.country+"/")
-    r = requests.get(company["csv"], allow_redirects=True)
-    open(self.HistoricalDirectory+self.country+"/"+company["symbol"]+".csv", 'wb').write(r.content)
+        print("Import complete, saved to : " + self.outputFile)
 
-  def DownloadAllHistorical(self):
-    if not (os.path.exists(self.HistoricalDirectory)):
-      os.mkdir(self.HistoricalDirectory)
-      os.mkdir(self.HistoricalDirectory+self.country+"/")
+    def DownloadHistorical(self, company):
+        historical_dir = os.path.join(self.HistoricalDirectory, self.country)  # Define full path
+        self._ensure_directory(historical_dir)  # Ensure the directory exists
 
-    for company in self.dataBase.values():
-      self.DownloadHistorical(company)
-      
-      print(company["symbol"]+" data downloaded")
-    
-    print("All historical data downloaded successfully to "+ self.HistoricalDirectory)
-    self.dataDownloaded=True
+        r = requests.get(company["csv"], allow_redirects=True)
+        open(os.path.join(historical_dir, company["symbol"] + ".csv"), 'wb').write(r.content)
 
-  def getHistoricalFileWithApi(self,symbol):
+    def DownloadAllHistorical(self):
+        for company in self.dataBase.values():
+            self.DownloadHistorical(company)
+            print(company["symbol"] + " data downloaded")
+
+        print("All historical data downloaded successfully to " + self.HistoricalDirectory)
+        self.dataDownloaded = True
+
+    def getHistoricalFileWithApi(self,symbol):
     company_data = requests.get(f"{self.HostURL}{self.performanceApi}{symbol}").json()
     company = None
     for c in company_data:
@@ -90,7 +86,7 @@ class MubasherAPI :
       return company["historicalFile"]
 
 
-  def getHistoricalFileWithScraping(self,url):
+    def getHistoricalFileWithScraping(self,url):
     html_doc =requests.get(self.HostURL+url)
     bs = BeautifulSoup(html_doc.text, "html.parser")
     historical_file = None
@@ -99,7 +95,7 @@ class MubasherAPI :
         historical_file= item["historical-data-url"]
     return historical_file
 
-  def getHistoricalFile(self,company):
+    def getHistoricalFile(self,company):
     historical_file = self.getHistoricalFileWithApi(company["symbol"])
     if not historical_file:
       historical_file = self.getHistoricalFileWithScraping(company["url"])
@@ -107,27 +103,30 @@ class MubasherAPI :
       raise Exception(f"Historical data for company with symbol {company['symbol']} cannot be imported")
     return historical_file
 
-  def LoadCompaines (self):
-    if not (os.path.exists(self.outputFile)):
-      print("Companies data has not been downloaded yet, starting downloading ..")
-      self._GetCompanies()
-    f = open(self.outputFile,'r')
-    raw_data = f.read()
-    self.dataBase={}
-    for item in xmltodict.parse (raw_data)["root"]["item"]:
-      self.dataBase[item["symbol"]["#text"]]={"name":item["name"]["#text"],
-                          "symbol":item["symbol"]["#text"],
-                          "url":item["url"]["#text"],
-                          "csv":item["csv"]["#text"]}
-    print("Companies data loaded successfully!")
-  def UpdatePrices(self):
+    
+    def LoadCompaines(self):
+        self._ensure_directory(self.CompaniesDirectory)  # Ensure the directory exists before loading
+        if not (os.path.exists(self.outputFile)):
+            print("Companies data has not been downloaded yet, starting downloading ..")
+            self._GetCompanies()
+        f = open(self.outputFile, 'r')
+        raw_data = f.read()
+        self.dataBase = {}
+        for item in xmltodict.parse(raw_data)["root"]["item"]:
+            self.dataBase[item["symbol"]["#text"]] = {"name": item["name"]["#text"],
+                                                     "symbol": item["symbol"]["#text"],
+                                                     "url": item["url"]["#text"],
+                                                     "csv": item["csv"]["#text"]}
+        print("Companies data loaded successfully!")
+
+    def UpdatePrices(self):
     self.lastPrices = requests.get(self.HostURL+self.PricesAPI,{"country": self.country}).json()
     print("Prices updated successfully to date " + self.lastPrices["lastUpdate"])
 
-  def ListCompanies(self):
+    def ListCompanies(self):
     table = tabulate([[company["symbol"],company["name"]]for company in self.dataBase.values()], headers=['Symbol', 'Name'], tablefmt='orgtbl')
     print(table)
-  def ListCountries(self):
+    def ListCountries(self):
     table = tabulate([["eg","Egypt"],
                       ["sa","Saudi Arabia"],
                       ["ae","Emirates"],
@@ -143,7 +142,7 @@ class MubasherAPI :
     print(table)
 
 
-  def GetHistoricalData(self,code,startDate,endDate):
+    def GetHistoricalData(self,code,startDate,endDate):
 
     if self.dataDownloaded:
       Data=pd.read_csv(self.HistoricalDirectory+self.country+"/"+code+".csv",header=None)
@@ -163,13 +162,13 @@ class MubasherAPI :
     return Data[lower_index:higher_index].values.tolist()
 
 
-  def _GetCompanyByCode(self,code,no_assert=False):
+    def _GetCompanyByCode(self,code,no_assert=False):
     if self.dataBase.keys().get(code,None)!= None:
       return self.dataBase[code]
     if no_assert:
       return None
     assert print("Invalid company code, please enter a valid company code")
-  def _FormatDate ( self,dateToFormat):
+    def _FormatDate ( self,dateToFormat):
 
     dateArray = dateToFormat.split("-")
     try:
@@ -180,7 +179,7 @@ class MubasherAPI :
       assert print ("Invalid date format, please enter a valid date")
     return returnDate
 
-  def SelectCountry(self,contry):
+    def SelectCountry(self,contry):
     pass
     if not contry in ["eg","sa","ae","bh","qa","om"]:
       assert print("please select a valid country")
@@ -190,7 +189,7 @@ class MubasherAPI :
     print("country set to "+country+ ", now getting new selection companies ..")
     self.LoadCompaines()
 
-  def PlotData(dataToPlot):
+    def PlotData(dataToPlot):
     fig = go.Figure(data=[go.Candlestick(x=dataToPlot[:][0],
                 open=dataToPlot[:][1],
                 high=dataToPlot[:][2],
@@ -199,7 +198,7 @@ class MubasherAPI :
 
     fig.show()
 
-  def getAllPrices(self,country):
+    def getAllPrices(self,country):
     response = requests.get(self.HostURL+self.PricesAPI,{"country": self.country}).json()
     date = response['lastUpdate']
     data = {'date':date,'prices':{}}
@@ -207,7 +206,7 @@ class MubasherAPI :
       data['prices'][company['code']]= [company['open'],company['high'],company['low'],company['value'],company['volume']]
     return data
 
-  def updateCompanies(self):
+    def updateCompanies(self):
     #get all prices first
     data = self.getAllPrices(self.country)
     for code,price in data['prices'].items():
